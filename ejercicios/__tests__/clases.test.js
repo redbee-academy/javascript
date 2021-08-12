df8c6ce80b916efb1702f76e77e7046f0efd0f1a
= <=>
2021-08-12 10:12:02 -0300

    resolucion clases

--- ejercicios/__tests__/clases.test.js
+++ ejercicios/__tests__/clases.test.js
@@ -4,6 +4,7 @@ it('crea una mascota', () => {
   const mascota = miMascota()
 
   expect(mascota instanceof Mascota).toBe(true)
+  expect(typeof mascota.nombre).toBe('string')
   expect(typeof mascota.color).toBe('string')
   expect(typeof mascota.hacerRuido()).toBe('string')
 })
--- ejercicios/clases.js
+++ ejercicios/clases.js
@@ -6,11 +6,24 @@ class Mascota {
   nombre
 }
 
+class Gato extends Mascota {
+  constructor(nombre, color) {
+    super(nombre)
+    this.color = color
+  }
+
+  color
+
+  hacerRuido() {
+    return 'Miau'
+  }
+}
+
 // Una función que debe devolver una instancia de una clase que herede de mascota
 // La instancia debe tener una propiedad que sea color de tipo string
 //  y un método que sea hacerRuido() que devuelve un string
 function miMascota() {
-
+  return new Gato('Klaus', 'tricolor')
 }
 
 
